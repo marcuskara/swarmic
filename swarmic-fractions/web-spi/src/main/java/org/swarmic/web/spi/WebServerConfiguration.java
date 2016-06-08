8d2527e63e3d2159eefb4043c80481242a80d74b
Antoine Sabot-Durand <antoine@sabot-durand.net>
2016-06-08 15:35:56 +0200

    Correction to web-spi to support fat jar mode

--- swarmic-fractions/web-spi/src/main/java/org/swarmic/web/spi/WebServerConfiguration.java
+++ swarmic-fractions/web-spi/src/main/java/org/swarmic/web/spi/WebServerConfiguration.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * JBoss, Home of Professional Open Source
  * Copyright 2016, Red Hat, Inc., and individual contributors
  * by the @authors tag.
@@ -12,12 +12,15 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ */
 
 package org.swarmic.web.spi;
 
 import org.apache.tamaya.inject.api.Config;
 
+import javax.enterprise.inject.Vetoed;
+
+@Vetoed
 public class WebServerConfiguration {
     @Config(value = "webserver.port", defaultValue = "8080")
     private int webserverPort;
