86eebf328ee3613067c9fa4f509ebed24a8873d1
Antoine Sabot-Durand <antoine@sabot-durand.net>
2016-06-08 18:09:38 +0200

    Rename samples module in examples

--- swarmic-examples/camel-metrics/src/main/java/org/swarmic/samples/camel/UnreliableService.java
+++ swarmic-examples/camel-metrics/src/main/java/org/swarmic/samples/camel/UnreliableService.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * JBoss, Home of Professional Open Source
  * Copyright 2016, Red Hat, Inc., and individual contributors
  * by the @authors tag.
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ */
 
 package org.swarmic.samples.camel;
 
